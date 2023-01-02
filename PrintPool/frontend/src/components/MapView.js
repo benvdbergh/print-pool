b0b033ee07237a9e0d011907cca94c948494fbab
Ben Van den Bergh <ben.vandenbergh@dematic.com>
2023-01-02 23:49:32 +0100

    Workaround layout + include custom markers

--- PrintPool/frontend/src/components/MapView.js
+++ PrintPool/frontend/src/components/MapView.js
@@ -1,5 +1,4 @@
 import React from 'react';
-// import carLogo from './car-svgrepo-com.svg';
 import mapboxgl from 'mapbox-gl';
 import 'mapbox-gl/dist/mapbox-gl.css';
 
@@ -7,42 +6,106 @@ mapboxgl.accessToken = 'pk.eyJ1IjoiYmVudmRiZXJnaCIsImEiOiJjbGFrNjN0YnEwMjVqM3BrM
 
 
 class MapView extends React.PureComponent {
-  constructor(props) {
-    super(props);
-    this.state = this.props.viewState
-    this.workshops = this.props.workshops
-    this.mapContainer = React.createRef();
-  }
-  componentDidMount() {
-    const { lng, lat, zoom } = this.state;
-    const map = new mapboxgl.Map({
-      container: this.mapContainer.current,
-      style: 'mapbox://styles/mapbox/streets-v12',
-      center: [lng, lat],
-      zoom: zoom
-    });
+	constructor(props) {
+		super(props);
+		this.state = this.props.viewState
+		this.workshops = this.props.workshops
+		this.mapContainer = React.createRef();
+	}
+	componentDidMount() {
+		const { lng, lat, zoom } = this.state;
+		const map = new mapboxgl.Map({
+			container: this.mapContainer.current,
+			style: 'mapbox://styles/mapbox/streets-v12',
+			center: [lng, lat],
+			zoom: zoom
+		});
 
-    map.on('move', () => {
-      this.setState({
-      lng: map.getCenter().lng.toFixed(4),
-      lat: map.getCenter().lat.toFixed(4),
-      zoom: map.getZoom().toFixed(2)
-      });
-    });
+		map.on('move', () => {
+			this.setState({
+			lng: map.getCenter().lng.toFixed(4),
+			lat: map.getCenter().lat.toFixed(4),
+			zoom: map.getZoom().toFixed(2)
+			});
+		});
 
-    const marker = new mapboxgl.Marker()
-      .setLngLat([30.5, 50.5])
-      .setPopup(new mapboxgl.Popup().setHTML("<h1>Hello World!</h1>"))
-      .addTo(map);
-  }
+		const geolocate = new mapboxgl.GeolocateControl({
+			positionOptions: {
+				enableHighAccuracy: true
+			},
+			showUserLocation: true,
+			showUserHeading: false
+		})
+		
 
-  render() {
-    return (
-      <div>
-        <div ref={this.mapContainer} className="map-container" />
-      </div>
-    );
-  }
+		map.addControl(geolocate, 'bottom-right')
+		map.on('load', () => {
+			geolocate.trigger();
+
+			map.loadImage(
+				'https://docs.mapbox.com/mapbox-gl-js/assets/custom_marker.png',
+				(error, image) => {
+					if (error) throw error;
+						map.addImage('custom-marker', image);
+					// Add a GeoJSON source with 2 points
+					map.addSource('workshops', {
+						'type': 'geojson',
+						'data': {
+							'type': 'FeatureCollection',
+							'features': [
+								{
+									'type': 'Feature',
+									'geometry': {
+										'type': 'Point',
+										'coordinates': [ 4.0323, 51.913	]
+									}
+								},
+								{
+									'type': 'Feature',
+									'geometry': {
+										'type': 'Point',
+										'coordinates': [4.414, 50.776]
+									}
+								}
+							]
+						}
+					});
+
+					// Add a symbol layer
+					map.addLayer({
+						'id': 'points',
+						'type': 'symbol',
+						'source': 'workshops',
+						'layout': {
+							'icon-image': 'custom-marker',
+						}
+					});
+			});
+		});
+
+		map.addControl(new mapboxgl.NavigationControl(), 'bottom-right');
+
+		const marker = new mapboxgl.Marker()
+		marker.setLngLat([30.5, 50.5]);
+		marker.setPopup(new mapboxgl.Popup().setHTML("<h1>Hello World!</h1>"));
+		marker.addTo(map);
+
+
+
+		// map.on('mousemove', (e) => {
+		// 	document.getElementById('info').innerHTML =
+		// 	// `e.point` is the x, y coordinates of the `mousemove` event
+		// 	// relative to the top-left corner of the map.
+		// 	JSON.stringify(e.point) +
+		// 	'<br />' +
+		// 	// `e.lngLat` is the longitude, latitude geographical position of the event.
+		// 	JSON.stringify(e.lngLat.wrap());
+		// 	});
+	}
+
+	render() {
+		return <div ref={this.mapContainer} className="map-container" height="100%"/>;
+	}
 }
 
 export default MapView;
